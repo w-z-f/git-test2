edbfb7fa1ff525db215af6d415502560fa1ae312
w.z.f <1766259879@qq.com>
2022-01-14 17:21:17 +0800

    third commit

--- src/main/java/edu/tjdz/gittest/git/GetTest.java
+++ src/main/java/edu/tjdz/gittest/git/GetTest.java
@@ -5,6 +5,7 @@ public class GetTest {
     public static void main(String[] args){
         System.out.println("hello git!");
         System.out.println("2");
+        System.out.println("3");
 
     }
 
