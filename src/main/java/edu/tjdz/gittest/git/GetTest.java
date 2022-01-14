8b3e4d126ccd409bd765f0bba582abcd5dad8510
w.z.f <1766259879@qq.com>
2022-01-14 17:30:30 +0800

    hot-fix1 commit

--- src/main/java/edu/tjdz/gittest/git/GetTest.java
+++ src/main/java/edu/tjdz/gittest/git/GetTest.java
@@ -6,6 +6,7 @@ public class GetTest {
         System.out.println("hello git!");
         System.out.println("2");
         System.out.println("3");
+        System.out.println("hot-fix commit1");
 
     }
 
